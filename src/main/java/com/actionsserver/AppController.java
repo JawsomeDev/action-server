7e95394d89bede7ab8c6888e3775d32569e8c309
DeepSeeDev <hyuk2000s@naver.com>
2025-08-20 15:38:58 +0900

    first commit

---
+++ src/main/java/com/actionsserver/AppController.java
@@ -0,0 +1,14 @@
+package com.actionsserver;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+}
