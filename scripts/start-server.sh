a690f19091217ff2e16f10ad94f7814cd72dc4b2
DeepSeeDev <hyuk2000s@naver.com>
2025-09-03 12:30:06 +0900

    push

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "------------서버시작---------------"
-cd /home/ubuntu/action-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+docker stop github-action-server || true
+docker rm github-action-server || true
+docker pull 354918411439.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-server:latest
+docker run -d --name github-action-server -p 8080:8080 354918411439.dkr.ecr.ap-northeast-2.amazonaws.com/github-action-server:latest
 echo "------------서버 배포 끝------------"
